abb4559e84ff06aae933a571b26effe5e3e576af
jermbo <jermbo@cosmicstrawberry.com>
2019-01-29 16:20:38 -0500

    adding more styles

--- src/views/Lessons.vue
+++ src/views/Lessons.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="wrapper wrapper--lessons">
-    <h1>Lessons</h1>
-    <LessonList v-for="(lesson, index) in lessons" :key="index" :lesson="lesson"/>
+    <div class="welcome">
+      <div class="welcome-inner">
+        <h1 class="title">Welcome to Typer</h1>
+        <p
+          class="description"
+        >Typer is a tool to help you get better at typing HTML and CSS. There are a lot of rules and syntax you need to get used to and here you can practice.</p>
+        <p>Select a lesson below to get started.</p>
+      </div>
+      <LessonList v-for="(lesson, index) in lessons" :key="index" :lesson="lesson"/>
+    </div>
   </div>
 </template>
 
