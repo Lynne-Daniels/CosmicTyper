c27f6d5f3e859b781b9a8c6b29b2e52ba0f9b169
jermbo <jermbo@cosmicstrawberry.com>
2019-02-26 14:30:46 -0500

    switching back to vanilla js

--- src/components/Typer.vue
+++ src/components/Typer.vue
@@ -3,21 +3,32 @@
     <h1 class="title">{{lesson.title}}</h1>
     <p class="desc">{{lesson.desc}}</p>
     <div class="charactersWrap">
-      <div v-html="characters" v-on:keyup="typing($event)"></div>
+      <div v-html="characters"></div>
     </div>
   </div>
 </template>
 
 <script>
+const modifiers = ["CapsLock", "Shift", "Control", "Alt"];
+
 export default {
   name: "typer",
   props: {
     lesson: Object
   },
+  data() {
+    return {
+      index: 0,
+      lessonSplit: this.lesson.final.split("")
+    };
+  },
+
+  created() {
+    window.addEventListener("keyup", this.typing);
+  },
   computed: {
     characters() {
-      const letterArray = this.lesson.final.split("");
-      const spanList = letterArray
+      const spanList = this.lessonSplit
         .map(letter => `<span class="character">${letter}</span>`)
         .join("");
       return spanList;
@@ -25,7 +36,18 @@ export default {
   },
   methods: {
     typing(evt) {
-      console.log(evt.target);
+      const key = evt.key;
+
+      if (this.isModifier(key)) {
+        return;
+      }
+
+      if (key != "Enter" && key == this.lessonSplit[this.index]) {
+        console.log("that was the right key!");
+      }
+    },
+    isModifier(key) {
+      return modifiers.some(mod => mod == key);
     }
   }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
+// const path = require("path");
 
-module.exports = {
-  pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
-    }
-  },
+// module.exports = {
+//   pluginOptions: {
+//     "style-resources-loader": {
+//       preProcessor: "scss",
+//       patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
+//     }
+//   },
 
-  baseUrl: undefined,
-  outputDir: undefined,
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
+//   baseUrl: undefined,
+//   outputDir: undefined,
+//   assetsDir: undefined,
+//   runtimeCompiler: undefined,
+//   productionSourceMap: undefined,
+//   parallel: undefined,
 
-  css: {
-    sourceMap: true
-  }
-};
+//   css: {
+//     sourceMap: true
+//   }
+// };
